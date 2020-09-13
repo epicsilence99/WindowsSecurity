104436f3346a355c6128ee8c1d24caf502d004c2
Luke <lkuik@shaw.ca>
2020-09-13 18:00:44 -0500

    Adding new script for Defender Exclusions, and updating Readme

---
+++ DefenderAV/DefenderExclusions.ps1
@@ -0,0 +1,76 @@
+﻿# Set file name variable
+$filename = "DefenderExclusions.txt"
+$filepath = pwd
+
+# Setting ErrorAction Preference
+$ErrorActionPreference = "Stop"
+
+# Show execution of script has started
+Write-host "Script has started....." -ForegroundColor Green
+Start-Sleep -s 2
+
+# Show script is running pre-requisite checks
+Write-Host "Running pre-requisite checks....."
+Start-Sleep -s 2
+
+# Check to see script is being run as administrator to as it's required to see exclusions
+Write-Host "Checking for elevated permissions....."
+Start-Sleep -s 2 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] “Administrator”)) {
+Write-Warning "Script is not being run as administrator. Open powershell as administrator and run the script again."
+Break
+}
+else {
+Write-Host "Script is being run as administrator." -ForegroundColor Green
+}
+Start-Sleep -s 2
+
+# Set variable to see if Defender AV is enabled on machine and error out if it's not 
+$DefenderAv = Get-MpComputerStatus | Select-Object -ExpandProperty AntispywareEnabled
+Write-Host "Checking if Defender AV is enabled....."
+Start-Sleep -s 2
+If ($DefenderAv -ne 'true') {
+Write-Warning "Defender AV is not currently enabled. Please enable Defender AV and run the script again"
+Break
+}
+else {
+Write-Host "Defender AV is enabled." -ForegroundColor Green
+}
+
+Start-Sleep -Seconds 2
+
+# checks complete
+Write-Host "Pre-requisite checks complete" -ForegroundColor Green
+
+Start-Sleep -Seconds 2
+
+# Start writing exclusions to file....
+Write-host "Starting to write Defender exclusions to file....."
+
+# path exclusions 
+$DefenderExclusionsPath = Get-MpPreference | Select-Object -ExpandProperty ExclusionPath
+Write-Output "Defender Path Exclusions:" >> $filename
+Write-output $DefenderExclusionsPath >> $filename `r`n
+
+# process exclusions
+$DefenderExclusionsProcess = Get-MpPreference | Select-Object -ExpandProperty ExclusionProcess
+Write-Output "Defender Process Exclusions:" >> $filename
+Write-output "$DefenderExclusionsProcess" >> $filename `r`n
+
+# extensions exclusions
+$DefenderExclusionsExtension = Get-MpPreference | Select-Object -ExpandProperty ExclusionExtension
+Write-Output "Defender Extension Exclusions:" >> $filename
+Write-output "$DefenderExclusionsExtension" >> $filename `r`n
+Write-host "Exclusions added to file" -ForegroundColor Green
+
+Start-Sleep -Seconds 5
+
+# Where to find how to configure exclusions for Defender AV
+Write-Output "Where can I learn more?:" >> $filename 
+Write-Output "To learn more how to configure each type of exclusion visit https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-antivirus/configure-extension-file-exclusions-microsoft-defender-antivirus" >> $filename
+Write-Host "To learn more how to configure each type of exclusion visit https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-antivirus/configure-extension-file-exclusions-microsoft-defender-antivirus"
+Start-Sleep -Seconds 5
+
+# Writing output is complete and where to find the file
+Write-host "Script completed, you can find the output of the file located at $pwd\$filename" -ForegroundColor Green
--- DefenderAV/readme.md
+++ DefenderAV/readme.md
@@ -5,18 +5,36 @@ DefenderAV is a collection of useful powershell scripts relating to Defender AV 
 The purpose of this powershell file is to check your Defender settings and output the values to a file in the directory you run the script from. Each setting will have a configuration URL from Microsoft where you can learn more.
 It will run interactively so you can track the progress, and has error checking enabled for admin rights, and also will check if Defender AV is enabled. If either the script is not running as admin, or Defender AV is disabled it will terminate the script with a warning.
 
-# How to get started
+## How to get started
 ## Download file with powershell:
 `Invoke-WebRequest https://raw.githubusercontent.com/lkuik/WindowsSecurity/master/DefenderAV/DefenderSettings.ps1 -OutFile .\defendersettings.ps1`
 ## To run the file in powershell:
 `powershell  -ExecutionPolicy Bypass -File .\defendersettings.ps1`
 
 ## An example of a succesful run will look like the below:
-![](./images/SuccessScript.png?)
+![](./images/DefenderSettingsSuccessScript.png?)
 
 ## An example of the script failing the pre-requisite check for admin rights would look like below:
-![](./images/AdminCheckFailed.PNG?)
+![](./images/AdminCheckFailed.png?)
 
 ## The results of the output file with Defender settings looks like below, but results will vary based on your settings:
 ![](./images/DefenderSettings.png?)
+
+# DefenderExclusions.ps1
+The purpose of this powershell file is to check your Defender exclusions and output the exclusions to a file in the directory you run the script from.  It will run interactively so you can track the progress, and has error checking enabled for admin rights, and also will check if Defender AV is enabled. If either the script is not running as admin, or Defender AV is disabled it will terminate the script with a warning.
+
+## How to get started
+## Download file with powershell:
+`Invoke-WebRequest https://raw.githubusercontent.com/lkuik/WindowsSecurity/master/DefenderAV/DefenderExclusions.ps1 -OutFile .\defenderexclusions.ps1`
+## To run the file in powershell:
+`powershell  -ExecutionPolicy Bypass -File .\defenderexclusions.ps1`
+
+## An example of a succesful run will look like the below:
+![](./images/DefenderExclusionsSuccessScript.png?)
+
+## An example of the script failing the pre-requisite check for admin rights would look like below:
+![](./images/AdminCheckFailed.png?)
+
+## The results of the output file with Defender exclusions looks like below, but results will vary based on your exclusions:
+![](./images/DefenderExclusions.png?)
 
